b6c15fc8711063db8f7c1ac5cf9fff5690e83f7b
Tim Beyer <tim.beyer@gmail.com>
2015-02-12 21:56:50 +0100

    throw error whenever VNode or VText is missing
    not only when both are missing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var convertHTML = require('./lib/html-to-vdom');
 module.exports = function initializeConverter (dependencies) {
-    if (!dependencies.VNode && !dependencies.VText) {
+    if (!dependencies.VNode || !dependencies.VText) {
         throw new Error('html-to-vdom needs to be initialized with VNode and VText');
     }
     return convertHTML(dependencies.VNode, dependencies.VText);
