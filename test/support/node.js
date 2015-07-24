67acda8e2c656982cd46a8e4684fa739640486a2
Tim Beyer <tim.beyer@gmail.com>
2015-07-24 13:23:39 +0200

    don't complain about redefinition of should

--- test/support/node.js
+++ test/support/node.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
-var should = chai.should();
+var should = chai.should(); // jshint ignore:line
 
 chai.use(chaiAsPromised);
 
